d7c228f17fa8973614977dbdfdd5bf7011a58890
Vee Uye <vee.uye@gmail.com>
2022-06-26 12:04:35 +0100

    Refactor <Alert /> tests

--- src/__tests__/Alert.test.js
+++ src/__tests__/Alert.test.js
@@ -2,47 +2,57 @@ import React from "react";
 import { render } from "@testing-library/react";
 import Alert from "../components/Alert";
 
-describe("Alert error", () => {
+describe("Alert", () => {
   const validProps = {
-    message: "Error!",
+    alertFail: {
+      message: "Error!",
+    },
+    alertSuccess: {
+      message: "Success!",
+    },
+    alertNull: {
+      message: "",
+    },
   };
-  it("renders correctly", () => {
-    const { asFragment } = render(<Alert message={validProps.message} />);
+
+  it("does not render if message props is empty", () => {
+    const { asFragment } = render(
+      <Alert message={validProps.alertFail.message} />
+    );
+    const alert = asFragment();
+
+    expect(alert).toMatchSnapshot();
+  });
+
+  it("renders correctly when an error occurs", () => {
+    const { asFragment } = render(
+      <Alert message={validProps.alertFail.message} />
+    );
 
     expect(asFragment()).toMatchSnapshot();
   });
 
   it("displays an error message", () => {
-    const { getByText } = render(<Alert message={validProps.message} />);
+    const { getByText } = render(
+      <Alert message={validProps.alertFail.message} />
+    );
 
     expect(getByText(/Error/).textContent).toBe("Error!");
   });
-});
 
-describe("Alert success", () => {
-  const validProps = {
-    message: "Success!",
-  };
-  it("renders correctly", () => {
+  it("renders correctly when successful", () => {
     const { asFragment } = render(
-      <Alert message={validProps.message} success />
+      <Alert message={validProps.alertSuccess.message} success />
     );
 
     expect(asFragment()).toMatchSnapshot();
   });
 
   it("displays a success message", () => {
-    const { getByText } = render(<Alert message={validProps.message} />);
+    const { getByText } = render(
+      <Alert message={validProps.alertSuccess.message} />
+    );
 
     expect(getByText(/Success/).textContent).toBe("Success!");
-  });
-});
-
-describe("Alert", () => {
-  it("does not render an alert if message props is empty", () => {
-    const { asFragment } = render(<Alert message="" />);
-    const alert = asFragment();
-
-    expect(alert).toMatchSnapshot();
   });
 });
